ed37a8c8163038adbbc06db41371ccbb357b42dd
germanpadua <77020763+germanpadua@users.noreply.github.com>
2021-03-17 09:46:03 +0100

    Add files via upload

---
+++ Proyecto2/src/main/java/Proyecto2/Dice.java
@@ -0,0 +1,85 @@
+package Proyecto1;
+
+import java.util.Random;
+
+public class Dice {
+        
+    private final float NHANGARSPROB;
+    private final float NSHIELDSPROB;
+    private final float NWEAPONSPROB;
+    private final float FIRSTSHOTPROB;
+
+    private Random generator;
+    
+    
+
+    Dice(){
+        NHANGARSPROB= 0.25f;
+        NSHIELDSPROB= 0.25f;
+        NWEAPONSPROB= 0.33f;
+        FIRSTSHOTPROB= 0.5f;
+        this.generator = new Random();
+    }
+
+    int initWithNHangar(){
+        double prob = this.generator.nextDouble();
+
+        if(prob <= NHANGARSPROB){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
+    int initWithNWeapons(){
+        final float PROB_CASO1 = 1-2*NWEAPONSPROB;
+        final float PROB_CASO2 = PROB_CASO1 + ( (1 - PROB_CASO1) / 2 );
+        double prob = this.generator.nextDouble();
+
+        if(prob <= PROB_CASO1){
+            return 3;
+        }else if (prob <= PROB_CASO2){
+            return 1;
+        }else{
+            return 2;
+        }
+    }
+
+    int initWithNShields(){
+        double prob = this.generator.nextDouble();
+
+        if (prob <= NSHIELDSPROB){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
+    int whoStarts(int nPlayers){
+        int player = this.generator.nextInt(nPlayers);
+        player++;
+        return player;
+    }
+
+    GameCharacter firstShot(){
+        double prob = this.generator.nextDouble();
+        GameCharacter firstshot = GameCharacter.SPACESTATION;
+
+        if (prob > FIRSTSHOTPROB){
+            firstshot = GameCharacter.ENEMYSTARSHIP;
+        }
+
+        return firstshot;
+    }
+
+    boolean spaceStationMoves(float speed){
+        double prob = this.generator.nextDouble();
+        //Comprobar excepción??
+
+        if(prob <= speed){
+            return true;
+        }else{
+            return false;
+        }
+    }      
+}
---
+++ Proyecto2/src/main/java/Proyecto2/SuppliesPackage.java
@@ -0,0 +1,32 @@
+package Proyecto1;
+
+public class SuppliesPackage {
+    private float ammoPower;
+    private float fuelUnits;
+    private float shieldPower;
+    
+    SuppliesPackage(float ammoPower, float fuelUnits, float shieldPower){
+        this.ammoPower = ammoPower;
+        this.fuelUnits = fuelUnits;
+        this.shieldPower = shieldPower;
+    }
+
+    //Implementar como se explicó en clase??
+    SuppliesPackage(SuppliesPackage s){
+        this (s.ammoPower,s.fuelUnits,s.shieldPower);
+    }
+
+    float getAmmoPower(){ 
+        return ammoPower;
+    }
+        
+
+    float getFuelUnits(){
+        return fuelUnits;
+    }
+
+    float getShieldPower(){
+        return shieldPower;
+    }
+
+}
