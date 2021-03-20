1ee7978a68c26b8689dc6c1deacd157e866f7718
pedro-jgl <pedrojgl14@gmail.com>
2021-03-20 19:00:39 +0100

    Quitados algunos warnings

---
+++ deepspace/src/main/java/deepspace/WeaponType.java
@@ -0,0 +1,15 @@
+package deepspace;
+
+public enum WeaponType {
+    LASER(2.0f), 
+    MISSILE(3.0f), 
+    PLASMA(4.0f);
+    
+    private float power;
+    
+    WeaponType(float power) { this.power = power; }
+
+    float getPower() { return power; }
+
+
+}
