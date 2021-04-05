81656ef248ee40964111b4811ac3fbacf9f01308
pedro-jgl <pedrojgl14@gmail.com>
2021-04-05 13:27:11 +0200

    Implementada GameUniverse

--- src/main/java/deepspace/GameUniverse.java
+++ src/main/java/deepspace/GameUniverse.java
@@ -1,7 +1,91 @@
 package deepspace;
 
+import java.util.ArrayList;
+
 public class GameUniverse {
     private final int WIN = 10;
 
-    private 
+    private int turns;
+    private int currentStationIndex;
+    private Dice dice;
+    private SpaceStation currentStation;
+    private EnemyStarShip currentEnemy;
+    private ArrayList<SpaceStation> spaceStations;
+    private GameStateController gameState;
+    
+    GameUniverse(){
+        turns = 0;
+        dice = new Dice();
+        gameState = new GameStateController();
+
+    }
+    
+    public CombatResult combat(){
+        throw new UnsupportedOperationException();
+    }
+
+    CombatResult combat(SpaceStation station, EnemyStarShip enemy){
+        throw new UnsupportedOperationException();
+    }
+
+    public void discardHangar(){
+        if (getState() == GameState.INIT || getState() == GameState.AFTERCOMBAT)
+            currentStation.discardHangar();
+    }
+
+    public void discardShieldBooster(int i){
+        if (getState() == GameState.INIT || getState() == GameState.AFTERCOMBAT)
+            currentStation.discardShieldBooster(i);
+    }
+
+    public void discardShieldBoosterInHangar(int i){
+        if (getState() == GameState.INIT || getState() == GameState.AFTERCOMBAT)
+            currentStation.discardShieldBoosterInHangar(i);
+    }
+    
+    public void discardWeapon(int i){
+        if (getState() == GameState.INIT || getState() == GameState.AFTERCOMBAT)
+            currentStation.discardWeapon(i);
+    }
+
+    public void discardWeaponInHangar(int i){
+        if (getState() == GameState.INIT || getState() == GameState.AFTERCOMBAT)
+            currentStation.discardShieldBoosterInHangar(i);
+    }
+    
+    public GameState getState(){
+        return gameState.getState();
+    }
+
+
+    public GameUniverseToUI getUIversion(){
+        return new GameUniverseToUI(this.currentStation, this.currentEnemy);
+    }
+
+    public boolean haveAWinner(){
+        if(currentStation.getNMedals() == WIN)
+            return true;
+        else
+            return false;
+    }
+
+
+    public void init(ArrayList<String> names){
+        throw new UnsupportedOperationException();
+    }
+
+    public void mountShieldBooster(int i){
+        if (getState() == GameState.INIT || getState() == GameState.AFTERCOMBAT)
+            currentStation.mountShieldBooster(i);
+    }
+
+    public void mountWeapon(int i){
+        if (getState() == GameState.INIT || getState() == GameState.AFTERCOMBAT)
+            currentStation.mountWeapon(i);
+    }
+    
+    public boolean nextTurn(){
+        throw new UnsupportedOperationException();
+    }
+
 }
