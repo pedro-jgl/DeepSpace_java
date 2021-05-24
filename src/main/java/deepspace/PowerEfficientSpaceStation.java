d07e0d729d8ce08a1d614618bbcbd3660ecdd71f
germanpadua <77020763+germanpadua@users.noreply.github.com>
2021-05-24 19:15:43 +0200

    Add files via upload

--- src/main/java/deepspace/PowerEfficientSpaceStation.java
+++ src/main/java/deepspace/PowerEfficientSpaceStation.java
@@ -20,8 +20,12 @@ public class PowerEfficientSpaceStation extends SpaceStation{
     @Override
     public Transformation setLoot(Loot loot){
         super.setLoot(loot);
-
-        return Transformation.NOTRANSFORM;
+        
+        if(loot.getEfficient())
+            return Transformation.GETEFFICIENT;
+        else
+            return Transformation.NOTRANSFORM;
+    
     }
 
     @Override
